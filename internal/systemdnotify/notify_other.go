9f13cd5a69db31b3a4e01999844834e99fa65336
paulohspred <100045106+paulohspred@users.noreply.github.com>
2026-09-05 14:58:45 -0300

    style: gofmt non-linux systemd notifier

--- internal/systemdnotify/notify_other.go
+++ internal/systemdnotify/notify_other.go
@@ -6,10 +6,28 @@ import "context"
 
 type Notifier struct{}
 
-func FromEnv() *Notifier                         { return &Notifier{} }
-func (n *Notifier) Enabled() bool                { return false }
-func (n *Notifier) WatchdogEnabled() bool        { return false }
-func (n *Notifier) Ready() error                 { return nil }
-func (n *Notifier) Stopping() error              { return nil }
-func (n *Notifier) Notify(string) error          { return nil }
+func FromEnv() *Notifier {
+	return &Notifier{}
+}
+
+func (n *Notifier) Enabled() bool {
+	return false
+}
+
+func (n *Notifier) WatchdogEnabled() bool {
+	return false
+}
+
+func (n *Notifier) Ready() error {
+	return nil
+}
+
+func (n *Notifier) Stopping() error {
+	return nil
+}
+
+func (n *Notifier) Notify(string) error {
+	return nil
+}
+
 func (n *Notifier) StartWatchdog(context.Context, func(error)) {}
